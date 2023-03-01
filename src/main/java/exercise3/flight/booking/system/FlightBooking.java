8160abdd347fab2b4cdc337c90279fe029a64110
prabhatram <prabhat.ram@oulu.fi>
2023-03-02 00:22:36 +0200

    Exercise 4 template repository added

---
+++ src/main/java/exercise3/flight/booking/system/FlightBooking.java
@@ -0,0 +1,5 @@
+package exercise3.flight.booking.system;
+
+public class FlightBooking {
+    
+}
---
+++ src/main/java/exercise3/flight/booking/system/Main.java
@@ -0,0 +1,10 @@
+package exercise3.flight.booking.system;
+
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
